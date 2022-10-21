b86ed938098a4f6189fe9a2167b466f364aaf7b4
rodrigoaraujosantos <araujo.1983@hotmail.com>
2022-10-21 11:55:55 -0300

    metodos

--- src/main/java/aulas/correcao/biblioteca/Autor.java
+++ src/main/java/aulas/correcao/biblioteca/Autor.java
@@ -1,4 +1,32 @@
 package aulas.correcao.biblioteca;
 
 public class Autor {
+    private String nome;
+    private String email;
+
+    public Autor(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Livro escreverLivro(String nome, double preco) {
+        Livro novoLivro = new Livro(nome, this, preco);
+        return novoLivro;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/aulas/correcao/biblioteca/Livro.java
+++ src/main/java/aulas/correcao/biblioteca/Livro.java
@@ -1,4 +1,41 @@
 package aulas.correcao.biblioteca;
 
+import java.util.ArrayList;
+
 public class Livro {
+    private String nome;
+    private Autor autor; // informações do autor
+    // composição
+    private double preco;
+
+    public Livro(String nome, Autor autor, double preco) {
+        this.nome = nome;
+        this.autor = autor;
+        this.preco = preco;
+    }
+
+    public static void main(String[] args) {
+        Autor jk = new Autor("JK", "jk@gmail.com");
+        Livro pedraFilosofal = new Livro("Harry Potter e a Pedra Filosofal", jk, 24.9);
+
+        System.out.println(pedraFilosofal.autor.getNome());
+        System.out.println(pedraFilosofal.autor.getEmail());
+
+        Livro camaraSecreta = jk.escreverLivro("Harry Potter e a Câmara Secreta", 30.0);
+
+        Livro prisioneiroAzk = jk.escreverLivro("Harry Potter e o prisioneiro de Azkaban", 40.0);
+
+        jk.setNome("BATATA");
+
+        System.out.println(pedraFilosofal.autor.getNome());
+        System.out.println(camaraSecreta.autor.getNome());
+        System.out.println(prisioneiroAzk.autor.getNome());
+
+        ArrayList<Livro> livrosJk = new ArrayList<>();
+        livrosJk.add(pedraFilosofal);
+        livrosJk.add(camaraSecreta);
+        livrosJk.add(prisioneiroAzk);
+
+        System.out.println(livrosJk);
+    }
 }
